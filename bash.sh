c0b1dee5cbc8543236cb8a11a661e460e8bd691c
521wangdong <tufuwd@163.com>
2021-04-28 11:51:36 +0800

    更新了一些已知问题

---
+++ bash.sh
@@ -0,0 +1,3 @@
+git add -A
+git commit -m '更新了一些已知问题'
+git push
--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-function add(x, y) {
-    return x+y
-
+function zoina() {
+    console.log('use zoina JS库') 
+}
+zoina.trim = function (str) {
+    return str.replace(/\s*/g, "");
+}
+zoina.default = function (x, y) {
+    return x + y;
 }
 
-export default add
+
+
+export default zoina
